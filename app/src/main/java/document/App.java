cea5ab45db6e53ca59d8d6ef1be795302ff0a2ff
jihwooon <jihwooon@gmail.com>
2022-06-28 20:40:22 +0900

    spring setting

--- app/src/main/java/document/App.java
+++ app/src/main/java/document/App.java
@@ -3,6 +3,9 @@
  */
 package document;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App {
     public String getGreeting() {
         return "Hello World!";
