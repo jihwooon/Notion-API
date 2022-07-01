129c3c9d3352c6937c06411eab28ba4aa501d255
jihwooon <jihwooon@gmail.com>
2022-07-01 10:14:03 +0900

    DatabaseService 생성하기

--- app/src/main/java/notion/databases/controller/DatabaseController.java
+++ app/src/main/java/notion/databases/controller/DatabaseController.java
@@ -1,8 +1,10 @@
 //RestController
 //@RequestMapping 추가하기
+//DatabaseService -> Database
 package notion.databases.controller;
 
 import notion.config.NotionConfigProperties;
+import notion.databases.service.DatabaseService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,14 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class DatabaseController {
 
     private final NotionConfigProperties notionConfigProperties;
+    private final DatabaseService databaseService;
 
-    public DatabaseController(NotionConfigProperties notionConfigProperties) {
+    public DatabaseController(NotionConfigProperties notionConfigProperties, DatabaseService databaseService) {
         this.notionConfigProperties = notionConfigProperties;
+        this.databaseService = databaseService;
     }
 
     @GetMapping("/notion")
-    public String getNotion() {
-        return "안녕하세요";
+    public String getNotions() {
+        return databaseService.getNotions();
     }
 
 }
